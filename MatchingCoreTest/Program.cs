0960c4705f18b724d4c560a44e4beedaa5e526c9
Hans Wong <hans.sy.wong@qq.com>
2018-03-05 14:10:09 +0800

    updates

--- MatchingCoreTest/Program.cs
+++ MatchingCoreTest/Program.cs
@@ -4,13 +4,172 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MatchingLib;
+using RabbitMQ.Client.Events;
+using Newtonsoft.Json;
 
 namespace MatchingCoreTest
 {
+    class Resp: ProcessOrderResult
+    {
+        public void ResetObj()
+        {
+            this.Success = false;
+            this.Comment = string.Empty;
+        }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+    class Tx : Transaction
+    {
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+    class Req : RequestToMatching { }
+    class ReqPool
+    {
+        static objPool<Req> Pool = new objPool<Req>(() => new Req() { order = new Order() });
+        public static Req Checkout()
+        {
+            return Pool.CheckoutMT();
+        }
+        public static void Checkin(Req req)
+        {
+            req.order.ResetObj();
+            Pool.Checkin(req);
+        }
+    }
+    class TxPool
+    {
+        static objPool<Tx> Pool = new objPool<Tx>(() => new Tx());
+        public static Tx Checkout()
+        {
+            return Pool.CheckoutMT();
+        }
+        public static void Checkin(Tx tx)
+        {
+            tx.ResetObj();
+            Pool.Checkin(tx);
+        }
+    }
+    class RespPool
+    {
+        static objPool<Resp> Pool = new objPool<Resp>(() => new Resp() { order = new Order() });
+        public static Resp Checkout()
+        {
+            return Pool.CheckoutMT();
+        }
+        public static void Checkin(Resp resp)
+        {
+            resp.order.ResetObj();
+            resp.ResetObj();
+            Pool.Checkin(resp);
+        }
+    }
     class Program
     {
+        static RabbitMqOut requestOut { get; set; }
+        static RabbitMqIn RespIn { get; set; }
+        static RabbitMqIn TxIn { get; set; }
+        List<string> users = new List<string>();
         static void Main(string[] args)
         {
+            string mqUri = "amqp://exchange:exchange@192.168.100.80:5672/";
+            requestOut = new RabbitMqOut(mqUri, "ReqToMatchingCore");
+            RespIn = new RabbitMqIn(mqUri, "RespFromMatchingCore");
+            TxIn = new RabbitMqIn(mqUri, "TxFromMatchingCore");
+
+            RespIn.BindReceived(MqRespInHandler);
+            TxIn.BindReceived(MqTxInHandler);
+            //Console.ReadKey();
+            //Req req = ReqPool.Checkout();
+            //req.order.et = Order.ExecutionType.Limit;
+            //req.order.id = Guid.NewGuid().ToString("N");
+            //req.order.u = Guid.NewGuid().ToString("N");
+            //req.order.t = Order.OrderType.Sell;
+            //req.order.p = 850;
+            //req.order.v = 500;
+            //requestOut.Enqueue(req);
+            //ReqPool.Checkin(req);
+            InitOrders();
+            Console.ReadKey();
+            requestOut.Shutdown();
+            RespIn.Shutdown();
+            TxIn.Shutdown();
+        }
+        private static void InitOrders()
+        {
+            int gType = 0;
+            {
+                ParallelOptions option = new ParallelOptions();
+                option.MaxDegreeOfParallelism = 8;
+                Parallel.For(0, 5000000, option, item =>
+                {
+                    Req req;
+                    Random rand = new Random();
+                    int type = gType;
+                    gType = gType == 0 ? 1 : 0;
+                    req = ReqPool.Checkout();
+                    if (type == 0)
+                    {
+                        int rangeRand = rand.Next(-1, 10000) + 1;
+                        double price = 950.00 - (double)rangeRand / 100.00;
+                        //price = decimal.Add(price, decimal.Divide(new decimal(rand.Next(0, int.MaxValue) + 1), new decimal(1000000000)));
+                        //price = decimal.Add(new decimal(1040.00), decimal.Divide(new decimal(rangeRand), new decimal(100)));
+                        long volume = (long)rand.Next(1, int.MaxValue);
+                        long user = rand.Next(1, 10000);
+                        string ticket = string.Format("{0}{1}", DateTime.Now.ToString("yyyyMMdd"), Guid.NewGuid().ToString("N"));
+                        req.order.p = price;
+                        req.order.v = volume;
+                        req.order.u = user.ToString();
+                        req.order.id = ticket;
+                        req.order.t = type == 0 ? Order.OrderType.Buy : Order.OrderType.Sell;
+                    }
+                    else
+                    {
+                        int rangeRand = rand.Next(-1, 10000) + 1;
+                        double price = 950.01 + (double)rangeRand / 100.00;
+                        //decimal price = new decimal();
+                        //price = decimal.Add(price, decimal.Divide(new decimal(rand.Next(0, int.MaxValue) + 1), new decimal(1000000000)));
+                        //price = decimal.Subtract(new decimal(1000.00), decimal.Divide(new decimal(rangeRand), new decimal(100)));
+                        long volume = (long)rand.Next(1, int.MaxValue);
+                        long user = rand.Next(1, 10000);
+                        string ticket = string.Format("{0}{1}", DateTime.Now.ToString("yyyyMMdd"), Guid.NewGuid().ToString("N"));
+                        req.order.p = price;
+                        req.order.v = volume;
+                        req.order.u = user.ToString();
+                        req.order.id = ticket;
+                        req.order.t = type == 0 ? Order.OrderType.Buy : Order.OrderType.Sell;
+                    }
+                    requestOut.Enqueue(req);
+                    ReqPool.Checkin(req);
+                }
+                );
+            }
+        }
+
+        private static void MqTxInHandler(object sender, BasicDeliverEventArgs e)
+        {
+            var bytes = e.Body;
+            Tx tx = TxPool.Checkout();
+            tx.FromBytes(bytes);
+            Console.WriteLine(tx.ToString());
+            TxPool.Checkin(tx);
+            TxIn.MsgFinished(e);
+        }
+
+        private static void MqRespInHandler(object sender, BasicDeliverEventArgs e)
+        {
+            var bytes = e.Body;
+            Resp resp = RespPool.Checkout();
+            resp.FromBytes(bytes);
+            Console.WriteLine(resp.ToString());
+            RespPool.Checkin(resp);
+            RespIn.MsgFinished(e);
         }
     }
 }
